2b86fc12b9756af44c1afe657986a699f16cbb62
Mihaela Macari <mikaela.malanici@gmail.com>
2025-02-13 20:14:32 +0200

    Fix "Select All" checkbox logic in AppHeader component.

--- src/components/layouts/appHeader/AppHeader.tsx
+++ src/components/layouts/appHeader/AppHeader.tsx
@@ -35,7 +35,10 @@ const AppHeader = () => {
     <div className="bg-white h-20 w-full border-solid border-b border-bg-gray-300 flex items-center">
       <div className="flex items-center gap-2">
         <Checkbox
-          checked={selectedFiles.length === files.length}
+          checked={
+            selectedFiles.length > 0 &&
+                        selectedFiles.length === files.length
+          }
           onCheckedChange={(event: CheckedState) =>
             handleSelectAllFiles(event)
           }
